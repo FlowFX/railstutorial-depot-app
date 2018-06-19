945b60c213c8ee878767defcf47765c209b4bbf7
Florian Posdziech <hallo@flowfx.de>
2018-06-19 07:52:17 +0200

    Finish Chapter 10

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_cart
+    Cart.new
+  end
+
+  test 'adding a product adds a line item' do
+    cart = new_cart
+    product = products(:python)
+
+    cart.add_product(product)
+
+    assert cart.valid?
+    assert_equal cart.line_items.length, 1
+    assert_equal cart.line_items.last.product_price, product.price
+  end
+
+  test 'adding a product twice results in only one line item' do
+    cart = new_cart
+    product = products(:ruby)
+
+    cart.add_product(product)
+    cart.save
+    cart.add_product(product)
+    cart.save
+
+    assert cart.valid?
+    assert_equal 1, cart.line_items.length
+  end
 end

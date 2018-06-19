6e779b63ce414fa2d106ef7e2a499cefcb33cb3b
Florian Posdziech <hallo@flowfx.de>
2018-06-19 08:41:48 +0200

    Add cart in sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   include Counter
+  include CurrentCart
+
+  before_action :set_cart
   before_action :get_counter, only: [:index]
   after_action  :increment_counter, only: [:index]
 

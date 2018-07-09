2256955c10b9cff46e916b4aabe672a2d9713d69
Florian Posdziech <hallo@flowfx.de>
2018-07-09 08:19:35 +0200

    Finish Chapter 14

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+
   def new
   end
 
   def create
     user = User.find_by(name: params[:name])
-    if user&.authenticate(params[:password])
+    if User.count.zero?
+      session[:user_id] = 42
+      redirect_to admin_url
+    elsif user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   include Counter
   include CurrentCart
 
+  skip_before_action :authorize
   before_action :set_cart
   before_action :get_counter, only: [:index]
   after_action  :increment_counter, only: [:index]
